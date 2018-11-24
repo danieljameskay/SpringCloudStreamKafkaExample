da2a73d6d59de66d96cff750a3da349f77037124
Daniel <danieljameskay@gmail.com>
2018-11-24 11:32:00 +0000

    Initial commit

---
+++ src/main/kotlin/com/example/SpringCloudStreamKafkaExample/SpringCloudStreamKafkaExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.SpringCloudStreamKafkaExample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCloudStreamKafkaExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringCloudStreamKafkaExampleApplication>(*args)
+}
